161e337abdc9edbc5710eb01bcd852941626f5ea
Linh <nguyenthuonglinh@gmail.com>
2020-02-21 15:40:24 -0800

    Week 4 Assignment using jQuery to launch Reserve Modal and Login Modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-$ (function () {
-  $ ('.carousel').carousel ({interval: 2000});
-  $ ('#carouselButton').click (function () {
+
+
+$(function () {
+
+  // change image rotation from default 5 sec to 2 sec
+  $('.carousel').carousel({ interval: 2000 });
+  
+  // Single Carousel button toggling between play and pause
+  $ ('#carouselButton').click (function () { 
     if ($ ('#carouselButton').children ('i').hasClass ('fa-pause')) {
       $ ('.carousel').carousel ('pause');
       $ ('#carouselButton').children ('i').removeClass ('fa-pause');
@@ -11,4 +17,17 @@ $ (function () {
       $ ('#carouselButton').children ('i').addClass ('fa-pause');
     }
   });
+
+// launch Reserve Campsite modal upon button click
+  $('#reserveButton').click(function () {
+    $('#reserveModal').modal('show');
+    
+  })
+
+// launch Login Modal
+  $('#loginButton').click(function () {
+    $('#loginModal').modal('show');
+  })
+  
 });
+
